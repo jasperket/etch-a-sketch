76446fade1f260af0c6b001dcb767adfebe7acbc
jasperket <jasperkeith1.vallecera@gmail.com>
2023-03-07 21:43:31 +0800

    Made 16x16 square grid

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-for(let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    document.body.appendChild(square);
+const container = document.querySelector('#container');
+for(let i = 0; i < 16; i++) {
+    const rectangle = document.createElement('div');
+    rectangle.classList.add('rectangle');
+    for(let i = 0; i < 16; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        rectangle.appendChild(square);
+    }
+    container.appendChild(rectangle);
 }
