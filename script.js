4c49876295c546a6971cb29b1f26e1176413c48c
jasperket <jasperkeith1.vallecera@gmail.com>
2023-03-09 20:12:13 +0800

    Added a button to make a new grid
    The button will make a new grid based on the input length.

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-const container = document.querySelector('#container');
-for(let i = 0; i < 16; i++) {
-    const rectangle = document.createElement('div');
-    rectangle.classList.add('rectangle');
-    for(let i = 0; i < 16; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        rectangle.appendChild(square);
+makeGrid(16);
+function makeGrid(side) {
+    const container = document.querySelector('#container');
+    const rectangles = document.querySelectorAll('.rectangle');
+    if(rectangles.length !== 0) {
+        rectangles.forEach(rect => {
+            container.removeChild(rect);
+        })
     }
-    container.appendChild(rectangle);
+    for(let i = 0; i < side; i++) {
+        const rectangle = document.createElement('div');
+        rectangle.classList.add('rectangle');
+        rectangle.style.height = 100/side + '%';
+        for(let i = 0; i < side; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            rectangle.appendChild(square);
+        }
+        container.appendChild(rectangle);
+    }
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'red';
+        })
+    })
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'red';
-    })
+const newGrid = document.querySelector('button');
+newGrid.addEventListener('click', () => {
+    const side = prompt('What is the length of the new grid?');
+    if(side > 0 && side < 100) {
+        makeGrid(side);
+    } else {
+        alert('Invalid value for length!');
+    }
 })
